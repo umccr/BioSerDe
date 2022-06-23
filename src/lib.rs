a54cb524404ec57ca5e4d4d264e8e3e4446c9197
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2022-06-23 17:13:46 -0300

    wip: problems with generic arquitecture

--- src/lib.rs
+++ src/lib.rs
@@ -17,35 +17,48 @@ use noodles::bed::Writer as BedWriter;
 // use noodles::fastq::AsyncReader as FastqReader;
 // use noodles::vcf::AsyncReader as VcfReader;
 
-// Refer to https://serde.rs/custom-serialization.html for details on
-// Custom Serialization.
-pub trait Serialize {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer;
-}
-struct ModernBioFormat<F> {
-    writer: F,
-    destination: String,
-}
-
-impl Serialize for ModernBioFormat<F: BedWriter> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut bio = serializer.serialize_struct("ModernBioFormat", 2)?;
-        bio.serialize_field("format", &self.format)?;
-        bio.serialize_field("destination", &self.destination)?;
-        bio.end()
-    }
-}
-
-// impl Deserialize for TraditionalBioFormat {
-//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+// // Refer to https://serde.rs/custom-serialization.html for details on
+// // Custom Serialization.
+// pub trait Serialize {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
 //     where
-//         D: Deserializer,
+//         S: Serializer;
+// }
+// struct ModernBioFormat<F> {
+//     writer: F,
+//     destination: String,
+// }
+
+// // perae eu nem vi que erro que da tentano usar isso direto,
+// // parece que eu deveria tentar enfiar um BedWriter aqui e ver o que acontece
+// // impl Serialize for BedWriter {
+
+// // }
+
+// impl Serialize for ModernBioFormat<F: BedWriter> {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: Serializer,
 //     {
-//         deserializer.deserialize_struct("Color", &["r", "g", "b"], ColorVisitor)
+//         let mut bio = serializer.serialize_struct("ModernBioFormat", 2)?;
+//         // hmm... perae nao faz muito sentido pra mim isso ser de fato o writer
+//         // um writer em tese nao pode ser serializado.
+//         // deixa eu double checkar se isso pode ser talvez outra coisa
+//         // hmmm nao mas ta no self
+
+//         // ta infelizmente pra mim eu acho que eu vou precisar destruir isso aqui
+//         // antes de chegar perto de entender como/porque isso funcionaria
+//         bio.serialize_field("format", &self.format)?;
+//         bio.serialize_field("destination", &self.destination)?;
+//         bio.end()
 //     }
 // }
+
+// // impl Deserialize for TraditionalBioFormat {
+// //     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+// //     where
+// //         D: Deserializer,
+// //     {
+// //         deserializer.deserialize_struct("Color", &["r", "g", "b"], ColorVisitor)
+// //     }
+// // }
