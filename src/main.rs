a9db8950c2022daeb96e86a212c0dcfc9ee347a1
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2022-07-03 20:22:22 -0300

    Initial bed serde demonstration

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,24 @@
 
 use std::{fs::{File, self}, io::BufReader};
 
+use bed::Record;
 use noodles_bed as bed;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let src = "../data/bed/well_formed_sample.bed";
+    let src = "data/bed/well_formed_sample.bed";
 
     let mut reader = File::open(src).map(BufReader::new).map(bed::Reader::new)?;
     let reference = fs::read_to_string(src)?;
-    let serialized = bed::to_string(&reader)?;
 
-    assert_eq!(reference, serialized);
+    println!("{:?}", reference);
+
+    let bed_records: Vec<Record<3>> = bed::from_reader(&mut reader);
+    println!("{:?}", bed_records);
+
+    let bed_record_serialization= bed::to_string(&bed_records)?;
+    println!("{:#?}", bed_record_serialization);
+
+    // assert_eq!(reference, serialized);
 
     Ok(())
 }
